b32d98c5d29c732a0670fc5d934f06bb73d1ee1f
Aaron Panchal <aaron.panchal@gmail.com>
2020-03-13 13:59:05 -0700

    Generate Idea model
    - Run `rails generate model idea title:string body:text user:references`
    - Modify models to add associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_13_203657) do
+ActiveRecord::Schema.define(version: 2020_03_13_204613) do
+
+  create_table "ideas", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_ideas_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -20,4 +29,5 @@ ActiveRecord::Schema.define(version: 2020_03_13_203657) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "ideas", "users"
 end
