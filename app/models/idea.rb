34e0dab006b6d27c965ed0f55b30764800d5b2c3
Aaron Panchal <aaron.panchal@gmail.com>
2020-03-14 20:08:01 -0700

    Add with_voter scope on Idea model

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,10 @@
 class Idea < ApplicationRecord
   belongs_to :user
   has_many :votes
+
+  scope :with_voter, ->(user) {
+    # String interpolation is safe here because no user data is used.
+    joins("LEFT OUTER JOIN votes ON ideas.id = votes.idea_id AND votes.user_id = #{user.id}")
+      .select("ideas.*, votes.user_id as voted")
+  }
 end
