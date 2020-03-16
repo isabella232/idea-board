6076b12595591314a61ae67b64e491a191196913
Aaron Panchal <aaron.panchal@gmail.com>
2020-03-16 10:38:21 -0700

    Create idea_board pack
    - Set up basic app structure for main client app

---
+++ app/javascript/packs/idea_board.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import IdeaBoard from "../idea_board/components/App";
+
+ReactDOM.render(<IdeaBoard />, document.getElementById("idea-board-app"));
