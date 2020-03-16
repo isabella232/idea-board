a90db5aa2b5228e376435bf1487973ff31324e82
Aaron Panchal <aaron.panchal@gmail.com>
2020-03-16 10:48:41 -0700

    Install and use react-router-dom

--- app/javascript/packs/idea_board.js
+++ app/javascript/packs/idea_board.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import IdeaBoard from "../idea_board/components/App";
 
-ReactDOM.render(<IdeaBoard />, document.getElementById("idea-board-app"));
+ReactDOM.render(
+  <BrowserRouter>
+    <IdeaBoard />
+  </BrowserRouter>,
+  document.getElementById("idea-board-app")
+);
