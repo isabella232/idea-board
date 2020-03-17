207a34eb7ae437ee891e84a41d764bf5e5577d8b
Aaron Panchal <aaron.panchal@gmail.com>
2020-03-17 14:09:33 -0700

    Ensure graphiql loads
    - Fix manifest per: https://github.com/rmosolgo/graphiql-rails/issues/75
    - Nest graphql_controller in Api module
    - Clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/api/graphql"
   end
-  post "/graphql", to: "graphql#execute"
+
   namespace :api do
     resources :tokens, only: :create
+    post "/graphql", to: "graphql#execute"
   end
 
   get "*path", to: 'client#index'
