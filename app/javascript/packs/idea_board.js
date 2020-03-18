8cb034610cfc067e08e2d9fc167aca401f6ae988
Edgar Price <emersonpriceiv@gmail.com>
2020-03-18 16:18:50 -0500

    Install Apollo client. Add auth header and correct endpoint to client configuration.

--- app/javascript/packs/idea_board.js
+++ app/javascript/packs/idea_board.js
@@ -1,14 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 
 import AuthManager from "../shared/AuthManager";
 import IdeaBoard from "../idea_board/components/App";
 
+const client = new ApolloClient({
+  uri: "/api/graphql",
+  request: operation => {
+    const token = localStorage.getItem('ideas-token');
+    token && operation.setContext({
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+  }
+});
+
 ReactDOM.render(
   <BrowserRouter>
     <AuthManager>
-      <IdeaBoard />
+      <ApolloProvider client={client}>
+        <IdeaBoard />
+      </ApolloProvider>
     </AuthManager>
   </BrowserRouter>,
   document.getElementById("idea-board-app")
