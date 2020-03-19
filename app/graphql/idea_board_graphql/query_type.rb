235dae8052a48bcc2fa9d5a684d40ce684e788f7
Aaron Panchal <aaron.panchal@gmail.com>
2020-03-18 20:57:51 -0700

    Add ideas and idea(id) queries

--- app/graphql/idea_board_graphql/query_type.rb
+++ app/graphql/idea_board_graphql/query_type.rb
@@ -5,8 +5,21 @@ module IdeaBoardGraphql
     ###############################################
     field :ping, String, null: false, description: "Test Query"
 
+    field :ideas, [Types::Idea], null: false
+    field :idea, Types::Idea, null: false do
+      argument :id, GraphQL::Types::ID, required: true
+    end
+
     def ping
       "pong"
+    end
+
+    def ideas
+      Idea.all
+    end
+
+    def idea(id:)
+      Idea.find(id)
     end
   end
 end
