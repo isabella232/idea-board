be1251a4a2742cb55beda9f545502317a2f754be
Aaron Panchal <aaron.panchal@gmail.com>
2020-03-19 11:04:20 -0700

    Disable vote button if not logged in

--- app/javascript/idea_board/components/VoteButton.js
+++ app/javascript/idea_board/components/VoteButton.js
@@ -3,16 +3,22 @@ import PropTypes from "prop-types";
 import { Button } from "reactstrap";
 import { Check, ThumbsUp } from "react-feather";
 
-const VoteButton = ({ voteCount, onClick, voted }) => (
-  <Button
-    color={voted ? "secondary" : "primary"}
-    onClick={voted ? () => {} : onClick}
-    disabled={voted}
-  >
-    <span className="mr-2">{voted ? <Check /> : <ThumbsUp />}</span>
-    {voteCount}
-  </Button>
-);
+import { useAuth } from "../../shared/AuthManager";
+
+const VoteButton = ({ voteCount, onClick, voted }) => {
+  const { isAuthenticated } = useAuth();
+
+  return (
+    <Button
+      color={voted ? "secondary" : "primary"}
+      onClick={onClick}
+      disabled={voted || !isAuthenticated}
+    >
+      <span className="mr-2">{voted ? <Check /> : <ThumbsUp />}</span>
+      {voteCount}
+    </Button>
+  );
+};
 
 VoteButton.propTypes = {
   voteCount: PropTypes.number.isRequired,
