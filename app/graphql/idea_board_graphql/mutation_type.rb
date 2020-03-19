c3365620bbe7450f84e41a006b1dafe4266be5a9
Aaron Panchal <aaron.panchal@gmail.com>
2020-03-19 10:10:48 -0700

    Improve example mutation

--- app/graphql/idea_board_graphql/mutation_type.rb
+++ app/graphql/idea_board_graphql/mutation_type.rb
@@ -3,13 +3,15 @@ module IdeaBoardGraphql
     ###############################################
     # Sample/Test API
     ###############################################
-    field :ping, GraphQL::Types::String, null: false, description: "Test Mutation"
+    field :ping, GraphQL::Types::String, null: false, description: "Test Mutation" do
+      argument :pong, GraphQL::Types::String, required: false, default_value: "pong"
+    end
 
     field :create_idea, mutation: Mutations::CreateIdea
     field :vote_for, mutation: Mutations::VoteForIdea
 
-    def ping
-      "pong"
+    def ping(pong:)
+      pong
     end
   end
 end
