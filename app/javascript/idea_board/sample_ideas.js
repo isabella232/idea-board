cf5c23922e04a4026d51b965f7b5983d4d8fcd9d
Aaron Panchal <aaron.panchal@gmail.com>
2020-03-19 09:42:55 -0700

    Revise sample ideas

--- app/javascript/idea_board/sample_ideas.js
+++ app/javascript/idea_board/sample_ideas.js
@@ -1,23 +1,23 @@
 export default [
   {
-    id: 1,
+    id: "1",
     title: "Stub Idea 1",
     body: "A new big idea!!!",
-    voteCount: 5,
+    votes: 5,
     voted: false
   },
   {
-    id: 2,
+    id: "2",
     title: "Stub Idea 2",
     body: "Another new big idea!!!",
-    voteCount: 7,
+    votes: 7,
     voted: true
   },
   {
-    id: 3,
+    id: "3",
     title: "Stub Idea 3",
     body: "ANOTHER new big idea!!!",
-    voteCount: 3,
+    votes: 3,
     voted: true
   }
 ];
