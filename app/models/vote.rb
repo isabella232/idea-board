adf6a4da5e72ca6f8f46e1f767d3de5cffd428eb
Aaron Panchal <aaron.panchal@gmail.com>
2020-03-18 21:41:01 -0700

    Add better uniqueness validation on votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,6 @@
 class Vote < ApplicationRecord
   belongs_to :idea, counter_cache: true
   belongs_to :user
+
+  validates :user, uniqueness: { scope: :idea, message: "has already voted for this idea" }
 end
